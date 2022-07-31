051204cfdf361597e1bdfb9de874fc4b3cef8eee
NifleySnifley <sailzforjunkmailz@gmail.com>
2022-07-31 18:44:45 -0500

    Added font awesome icons

--- seeker_frontend/src/App.js
+++ seeker_frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
 function App() {
 	return (
 		<div className='App'>
-			<p>Hello!</p>
+			<p>Hello!</p><br/>
+			Ready for some <FontAwesomeIcon icon={Icons.faRocket}/>?
 		</div>
 	);
 }
