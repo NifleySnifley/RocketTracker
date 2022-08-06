13ffee282d314fd229ad57ce98f3e420e5c09f04
NifleySnifley <sailzforjunkmailz@gmail.com>
2022-08-06 12:21:02 -0500

    Improved adjustable grid layout

--- seeker_frontend/src/components/Grid.js
+++ seeker_frontend/src/components/Grid.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-no-bind */
 import React, {createContext, useRef, useState} from 'react';
+import GridAdjuster from './GridAdjuster';
 
 const defaultGridState = () => ({
 	width: document.documentElement.clientWidth,
@@ -16,11 +18,40 @@ const defaultGridState = () => ({
 	flex: 1,
 });
 
+function scaleLayout(layout) {
+	const newWH = {
+		width: document.documentElement.clientWidth,
+		height: document.documentElement.clientHeight,
+	};
+
+	const newDivs = {
+		x: layout.divisions.x.map(s => (s / layout.width) * newWH.width),
+		y: layout.divisions.y.map(s => (s / layout.height) * newWH.height),
+	};
+
+	console.log({...newDivs, ...newWH, flex: layout.flex});
+	return {divisions: newDivs, ...newWH, flex: layout.flex};
+}
+
+function loadLayout() {
+	if (window.localStorage.getItem('layout')) {
+		return JSON.parse(window.localStorage.getItem('layout'));
+	}
+
+	return defaultGridState();
+}
+
 // Add reactive bars to adjust dividers and flex direction
 export default function Grid(props) {
-	const [layout, setLayout] = useState(defaultGridState());
+	// eslint-disable-next-line react/hook-use-state
+	const [layout, setLayoutRaw] = useState(loadLayout());
 	const resizerAdded = useRef(false);
 	const divBar = useRef(null);
+
+	const setLayout = l => {
+		window.localStorage.setItem('layout', JSON.stringify(l));
+		setLayoutRaw(l);
+	};
 
 	const fixedChildren = React.Children.map(props.children, child => {
 		// Checking isValidElement is the safe way and avoids a typescript
@@ -34,7 +65,7 @@ export default function Grid(props) {
 
 	React.useEffect(() => {
 		function handleResize() {
-			setLayout(defaultGridState());
+			setLayout(scaleLayout(loadLayout()));
 		}
 
 		if (!resizerAdded.current) {
@@ -43,19 +74,45 @@ export default function Grid(props) {
 		}
 	});
 
+	/**
+	 * Called when a divider is dragged
+	 * @param {DragEvent} e
+	 * @param {number} dir
+	 * TODO: if dir is an array (path) use it for flex dividers
+	 */
+	function drag(e, dir) {
+		const newLayout = {...layout};
+		const dirs = ['y', 'x'];
+
+		if (Array.isArray(dir)) {
+			newLayout.divisions[dirs[1 - dir[0]]][dir[1]] = e[`client${dirs[1 - dir[0]].toUpperCase()}`];
+		} else {
+			newLayout.divisions[dirs[dir]] = [0, 0].map(v => e[`client${dirs[dir].toUpperCase()}`]);
+		}
+
+		setLayout(newLayout);
+	}
+
+	const adjusterSize = 4;
+
 	return (
 		<div>
-			<div
+			{fixedChildren}
+			<GridAdjuster layout={layout} size={adjusterSize} target={1 - layout.flex} onAdjust={drag}/>
+			<GridAdjuster layout={layout} size={adjusterSize} target={[layout.flex, 0]} onAdjust={drag}/>
+			<GridAdjuster layout={layout} size={adjusterSize} target={[layout.flex, 1]} onAdjust={drag}/>
+			{/* <div
 				ref={divBar}
+				draggable
 				className='grid-adjuster'
 				style={{
 					position: 'absolute',
-					left: layout.flex === 0 ? 0 : layout.divisions.x[0],
-					top: layout.flex === 1 ? 0 : layout.divisions.y[0],
-					width: layout.flex === 0 ? layout.width : 0,
-					height: layout.flex === 1 ? layout.height : 0,
-				}}/>
-			{fixedChildren}
+					left: layout.flex === 0 ? 0 : layout.divisions.x[0] - adjusterSize,
+					top: layout.flex === 1 ? 0 : layout.divisions.y[0] - adjusterSize,
+					width: layout.flex === 0 ? layout.width : adjusterSize * 2,
+					height: layout.flex === 1 ? layout.height : adjusterSize * 2,
+				}}
+				onDragEnd={e => drag(e, layout.flex)}/> */}
 		</div>
 	);
 }
--- seeker_frontend/src/components/MapRenderer.js
+++ seeker_frontend/src/components/MapRenderer.js
@@ -11,40 +11,44 @@ export default function MapRenderer() {
 
 	// TODO: Global state for sensor data and events
 	useEffect(() => {
-		if (map.current) {
-			return;
-		}
-
-		map.current = new maplibregl.Map({
-			container: mapContainer.current,
-			style: `https://api.maptiler.com/maps/streets/style.json?key=${API_KEY}`,
-			center: [lng, lat],
-			zoom,
-		});
-
-		map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
-
-		map.current.on('load', () => {
-			map.current.addSource('wms-gisdata-mn', {
-				type: 'raster',
-				// https://maplibre.org/maplibre-gl-js-docs/style-spec/sources/
-				tiles: [
-					// 'https://imageserver.gisdata.mn.gov/cgi-bin/mncomp?bbox={bbox-epsg-3857}&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=mncomp',
-					'https://imageserver.gisdata.mn.gov/cgi-bin/wms?bbox={bbox-epsg-3857}&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=fsa2021',
-				],
-				tileSize: 256,
+		if (!map.current) {
+			map.current = new maplibregl.Map({
+				container: mapContainer.current,
+				style: `https://api.maptiler.com/maps/streets/style.json?key=${API_KEY}`,
+				center: [lng, lat],
+				zoom,
 			});
-			map.current.addLayer(
-				{
-					id: 'gisdata-mn',
+
+			map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
+			// Map.current.addControl(new maplibregl.GeolocateControl(), 'bottom-right');
+
+			map.current.on('load', () => {
+				map.current.addSource('wms-gisdata-mn', {
 					type: 'raster',
-					source: 'wms-gisdata-mn',
-					paint: {},
-				},
-				'aeroway_fill',
-			);
-		});
+					// https://maplibre.org/maplibre-gl-js-docs/style-spec/sources/
+					tiles: [
+					// 'https://imageserver.gisdata.mn.gov/cgi-bin/mncomp?bbox={bbox-epsg-3857}&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=mncomp',
+						'https://imageserver.gisdata.mn.gov/cgi-bin/wms?bbox={bbox-epsg-3857}&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=fsa2021',
+					],
+					tileSize: 256,
+				});
+
+				map.current.addLayer(
+					{
+						id: 'gisdata-mn',
+						type: 'raster',
+						source: 'wms-gisdata-mn',
+						paint: {},
+					},
+					'aeroway_fill',
+				);
+			});
+		}
 	});
+
+	if (map.current) {
+		map.current.resize();
+	}
 
 	return (
 		<div className='map-wrap'>
